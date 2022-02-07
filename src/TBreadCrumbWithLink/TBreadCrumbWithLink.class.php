e5884a8f57cab59ec71e04bdb8e398d57edfefbe
Marcelo Lopes <lopes.cte@gmail.com>
2022-02-07 01:13:09 -0300

    Update TBreadCrumbWithLink.class.php
    Fix namespace

--- src/TBreadCrumbWithLink/TBreadCrumbWithLink.class.php
+++ src/TBreadCrumbWithLink/TBreadCrumbWithLink.class.php
@@ -1,5 +1,5 @@
 <?php
-namespace Lopescte\Adianti-plugins\TBreadCrumbWithLink\;
+namespace Lopescte\AdiantiPlugins\TBreadCrumbWithLink;
 
 use Adianti\Widget\Util\TBreadCrumb;
 use Adianti\Widget\Base\TElement;
